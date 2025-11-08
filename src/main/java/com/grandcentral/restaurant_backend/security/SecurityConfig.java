a4bd951fc606ccaf2ce5cd571d59afdda466eb95
lu2311 <luisyraita2306@gmail.com>
2025-11-08 01:32:09 -0500

    correcion

--- src/main/java/com/grandcentral/restaurant_backend/security/SecurityConfig.java
+++ src/main/java/com/grandcentral/restaurant_backend/security/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("https://grand-central-frontend.onrender.com");
+        config.addAllowedOriginPattern("https://grand-central-backend-develop.onrender.com/");
         config.addAllowedOriginPattern("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
